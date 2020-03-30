a00ccbba3e185a701be9fa6585bfce40363e746a
MarlyssaB <marlyssa.boucher@gmail.com>
2020-03-30 01:49:14 -0500

    this is nowhere near what I wanted

--- ClientApp/src/app/item/item.component.ts
+++ ClientApp/src/app/item/item.component.ts
@@ -9,9 +9,12 @@ export class ItemComponent implements OnInit {
   @Input()  movie ;
   theStyles;
   name;
+  backDrop;
+  defaultImg;
   constructor() { }
 
   ngOnInit() {
+    const defaultImg = './download.png';
     const backDrop = 'http://image.tmdb.org/t/p/original' + this.movie.backdrop_path;
     this.theStyles = {width: '200px', backgroundImage: 'url(' + backDrop + ')'};
 
@@ -19,6 +22,12 @@ export class ItemComponent implements OnInit {
       this.name = this.movie.original_title;
     } else {
       this.name = this.movie.name;
+    }
+
+    if (!this.movie.backdrop_path) {
+      this.backDrop = this.defaultImg;
+    } else {
+      this.backDrop = this.movie.backDrop;
     }
   }
 
